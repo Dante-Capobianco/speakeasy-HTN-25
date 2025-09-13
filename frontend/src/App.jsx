a3a27c7cda000581403eadf77b2652ded0ee4a3c
Dante Capobianco <capodevservices@gmail.com>
2025-09-13 00:50:53 -0400

    Implement React frontend & clean up boiler code

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>This is root page of application; index.css is for styling to apply across entire website; app.css for page-specific styling</div>
+    </>
+  )
+}
+
+export default App
